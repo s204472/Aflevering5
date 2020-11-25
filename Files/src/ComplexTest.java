e355fa0a7e03bf89ca64e4e69deab983dddfda8a
s204472 <m.ks@outlook.dk>
2020-11-25 18:51:38 +0100

    Complex test

--- Files/src/ComplexTest.java
+++ Files/src/ComplexTest.java
@@ -2,15 +2,23 @@
 public class ComplexTest {
 
 	public static void main(String[] args) {
+		// Initializing two complex numers.
 		Complex z1 = new Complex(1,2);
 		Complex z2 = new Complex(4,5);
 		
-		System.out.println(z1);
-		System.out.println(z2);
-		System.out.println(z1.plus(z2));
-		System.out.println(z1.times(z2));
+		System.out.println(z1); // Expected output 1.0+2.0i
+		System.out.println(z2); // Expected output 4.0+5.0i
 		
+		// Calculates (1.0+2.0i) + (4.0+5.0i) - Expected output is 5.0+7.0i
+		System.out.println(z1.plus(z2)); 
+		
+		// Calculates (1.0+2.0i) x (4.0+5.0i) - Expected output is -6.0+13.0i
+		System.out.println(z1.times(z2)); 
+		
+		// Copies a complex number
 		Complex z3 = new Complex(3,4);
+		
+		// Calculates absolute value of Complex number - Expected output is 5.0
 		System.out.println(z3.abs()); 
 	}
 
